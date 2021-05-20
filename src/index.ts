9de40ecfe22195f4fc79141f52c1ddcc9e0550e8
yangguanghe <1649063797@qq.com>
2021-05-21 01:48:37 +0800

    枚举的位运算

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,41 @@
-type Deck = NormalCard[]
-enum Color {
-    heart = "♥",
-    spade = "♠",
-    club = "♣",
-    diamond = "♦"
-
+// 用户权限
+enum Permission {
+    Read = 1,   // 0001
+    Write = 2,  // 0010
+    Create = 4, // 0100
+    Delete = 8  // 1000
 }
 
-enum Mark {
-    one = "A",
-    two = "2",
-    three = "3",
-    four = "4",
-    five = "5",
-    six = "6",
-    seven = "7",
-    eight = "8",
-    nine = "9",
-    ten = "10",
-    eleven = "J",
-    twelve = "Q",
-    king = "K"
-}
-type NormalCard = {
-    color: Color,
-    mark: Mark
-}
-function createDeck():Deck {
-    const deck: Deck = []
-    const mark = Object.values(Mark)
-    const color = Object.values(Color)
-    for (const m of mark) {
-        for (const c of color) {
-            deck.push({
-                color: c,
-                mark: m
-            })
-        }
-    }
-    return deck;
+// 1.如何组合权限
+// 使用或运算
+// 0001
+// 或
+// 0010
+// 结果
+// 0011
+
+let p:Permission = Permission.Read | Permission.Write;
+
+// 2.如何判断是否拥有某个权限
+// 0011
+// 且
+// 0010
+// 结果
+// 0010
+
+function hasPermission(target:Permission, per: Permission) {
+    return (target & per) === per
 }
 
-function printDeck(deck: Deck) {
-    let result = "\n";
-    deck.forEach(item => {
-        let str = item.color + item.mark
-        result += str + "\t"
-    })
-    console.log(result)
-}
+// 判断变量p是否拥有可读权限
+console.log(hasPermission(p, Permission.Read));
+console.log(hasPermission(p, Permission.Write));
+// 3.如何删除某个权限
+// 0011
+// 异或
+// 0010
+// 结果
+// 0001
 
-const deck = createDeck();
-printDeck(deck);
+p = p ^ Permission.Write
+console.log(hasPermission(p, Permission.Write))
