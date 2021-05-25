5b931e573cccc5feadc3599063bd72f320b82164
yangguanghe <1649063797@qq.com>
2021-05-25 23:15:34 +0800

    ts接口的使用

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,26 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-const func_1 = require("./func");
-const deck = func_1.createDeck();
-func_1.printDeck(deck);
+let u = {
+    name: 'yang',
+    age: 18,
+    sayHello: () => {
+        console.log('hello');
+    },
+    getName: () => {
+        console.log('yang');
+    }
+};
+function sum(numbers, callback) {
+    let s = 0;
+    numbers.forEach(n => {
+        if (callback(n)) {
+            s += n;
+        }
+    });
+    return s;
+}
+const result = sum([1, 3, 5, 7, 9], n => n % 2 !== 0);
+console.log(result);
+let t = {
+    T1: 'yang',
+    T2: 18,
+    T3: true
+};
