647a2710eabbc30f520b5c8720a9f75203aa0a7f
yangguanghe <1649063797@qq.com>
2021-05-31 23:39:41 +0800

    接口改造

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,4 @@
-let person = {
-    name: "伪装成鸭子的人",
-    age: 12,
-    sound: "嘎嘎嘎",
-    swin() {
-        console.log(this.name + "正在游泳，并发出" + this.sound + "的声音");
-    }
-};
-let d = person;
-d.swin();
-function sum(numbers, callback) {
-    let s = 0;
-    numbers.forEach((n, i) => {
-        if (callback(n, i)) {
-            s += n;
-        }
-    });
-    return s;
-}
-const result = sum([3, 4, 5, 7, 11], n => n % 2 !== 0);
-console.log(result);
+Object.defineProperty(exports, "__esModule", { value: true });
+const func_1 = require("./func");
+const deck = func_1.createDeck();
+func_1.printDeck(deck);
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,4 @@
-interface Duck {
-    sound: "嘎嘎嘎"
-    swin(): void
-}
+import { createDeck, printDeck } from "./func";
 
-// interface Person {
-//     name: string
-//     age: number
-//     sound: string
-//     swin(): void
-// }
-
-let person = {
-    name: "伪装成鸭子的人",
-    age: 12,
-    sound: "嘎嘎嘎" as "嘎嘎嘎",
-    swin() {
-        console.log(this.name + "正在游泳，并发出" + this.sound + "的声音");
-    }
-}
-
-let d:Duck = person
-
-d.swin()
-
-interface Condition {
-    (n: number, index: number): boolean
-}
-
-function sum(numbers: number[], callback:Condition) {
-    let s = 0;
-    
-    numbers.forEach((n, i) => {
-        if (callback(n, i)) {
-            s += n
-        }
-    })
-    return s;
-}
-
-const result = sum([3, 4, 5, 7, 11], n => n % 2 !== 0);
-
-console.log(result)
+const deck = createDeck();
+printDeck(deck);
