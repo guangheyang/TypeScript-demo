413449738ffbfedd1648e883ec94e1e0899b4880
yangguanghe <1649063797@qq.com>
2021-06-01 00:49:27 +0800

    访问器

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,38 @@
 class User {
     readonly id: number // 不能改变
-    name: string
-    age: number
     gender: '男' | '女' = "男"
     pid?: string
-    private publishNumber: number = 3
-    private curNumber: number = 0
+    private _publishNumber: number = 3
+    private _curNumber: number = 0
 
-    constructor(name: string, age: number, gender: '男' | '女' = '男') {
+    constructor(name: string, private _age: number, gender: '男' | '女' = '男') {
         this.id = Math.random();
-        this.name = name;
-        this.age = age;
     }
     publish(title: string) {
-        if (this.curNumber < this.publishNumber) {
+        if (this._curNumber < this._publishNumber) {
             console.log('发布一篇文章' + title)
-            this.curNumber++
+            this._curNumber++
         } else {
             console.log('你今日发布的文章数量以达到上限')
         }
+    }
+
+    set age(value: number) {
+        if (value < 0) {
+            this._age = 0
+        } else if (value >= 200) {
+            this._age = 200
+        } else {
+            this._age = value
+        }
+    }
+    get age() {
+        return Math.floor(this._age)
     }
 }
 
 const u = new User('yang', 18)
 
-u.publish('文章1')
-u.publish('文章2')
-u.publish('文章3')
-u.publish('文章4')
+u.age = 1.8
+
+console.log(u.age)
