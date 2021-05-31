dfaa3401843b6e8b621fa70808d5fe3931ef4e18
yangguanghe <1649063797@qq.com>
2021-06-01 00:33:24 +0800

    类语法

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,24 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-const func_1 = require("./func");
-const deck = func_1.createDeck();
-func_1.printDeck(deck);
+class User {
+    constructor(name, age, gender = '男') {
+        this.gender = "男";
+        this.publishNumber = 3;
+        this.curNumber = 0;
+        this.id = Math.random();
+        this.name = name;
+        this.age = age;
+    }
+    publish(title) {
+        if (this.curNumber < this.publishNumber) {
+            console.log('发布一篇文章' + title);
+            this.curNumber++;
+        }
+        else {
+            console.log('你今日发布的文章数量以达到上限');
+        }
+    }
+}
+const u = new User('yang', 18);
+u.publish('文章1');
+u.publish('文章2');
+u.publish('文章3');
+u.publish('文章4');
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,30 @@
-import { createDeck, printDeck } from "./func";
+class User {
+    readonly id: number // 不能改变
+    name: string
+    age: number
+    gender: '男' | '女' = "男"
+    pid?: string
+    private publishNumber: number = 3
+    private curNumber: number = 0
 
-const deck = createDeck();
-printDeck(deck);
+    constructor(name: string, age: number, gender: '男' | '女' = '男') {
+        this.id = Math.random();
+        this.name = name;
+        this.age = age;
+    }
+    publish(title: string) {
+        if (this.curNumber < this.publishNumber) {
+            console.log('发布一篇文章' + title)
+            this.curNumber++
+        } else {
+            console.log('你今日发布的文章数量以达到上限')
+        }
+    }
+}
+
+const u = new User('yang', 18)
+
+u.publish('文章1')
+u.publish('文章2')
+u.publish('文章3')
+u.publish('文章4')
