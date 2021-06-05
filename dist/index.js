5b30f56cfc9c7687f33d06cb1bd8ce31d2071074
yangguanghe <1649063797@qq.com>
2021-06-05 23:51:52 +0800

    自定义字典类

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,15 @@
-function mixinArray(arr1, arr2) {
-    if (arr1.length !== arr2.length) {
-        throw new Error("两个数组长度不一致");
-    }
-    let result = [];
-    for (let i = 0; i < arr1.length; i++) {
-        result.push(arr1[i]);
-        result.push(arr2[i]);
-    }
-    return result;
-}
-const result = mixinArray([1, 3, 4], ["a", "b", "c"]);
-result.forEach(r => {
-    console.log(r);
+Object.defineProperty(exports, "__esModule", { value: true });
+const dictionary_1 = require("./dictionary");
+const dic = new dictionary_1.Dictionary();
+dic.set("a", 1);
+dic.set("b", 2);
+dic.set("a", 12);
+dic.forEach((k, v) => {
+    console.log(`${k}: ${v}`);
 });
+console.log(dic.hasKey("c"));
+dic.delete("a");
+dic.forEach((k, v) => {
+    console.log(`${k}: ${v}`);
+});
+console.log(dic.length);
