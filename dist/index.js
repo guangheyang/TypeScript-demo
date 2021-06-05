345425cd92aa21bfd128442e42180169664df2b1
yangguanghe <1649063797@qq.com>
2021-06-05 18:38:08 +0800

    多泛型

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,15 @@
-function nameToUpperCase(obj) {
-    obj.name = obj.name
-        .split(' ')
-        .map(n => n[0].toUpperCase() + n.substr(1))
-        .join(" ");
-    return obj;
+function mixinArray(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        throw new Error("两个数组长度不一致");
+    }
+    let result = [];
+    for (let i = 0; i < arr1.length; i++) {
+        result.push(arr1[i]);
+        result.push(arr2[i]);
+    }
+    return result;
 }
-const o = {
-    name: 'guange yang',
-    age: 18,
-    gender: 'man'
-};
-console.log(nameToUpperCase(o));
+const result = mixinArray([1, 3, 4], ["a", "b", "c"]);
+result.forEach(r => {
+    console.log(r);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
-interface hasNameProperty {
-    name: string
+/**
+ * 将两个数组进行混合
+ */
+function mixinArray<T, K>(arr1: T[], arr2: K[]): (T | K)[] {
+    if (arr1.length !== arr2.length) {
+        throw new Error("两个数组长度不一致");
+    }
+    let result: (T | K)[] = [];
+    for (let i = 0; i < arr1.length; i++) {
+        result.push(arr1[i])
+        result.push(arr2[i]);
+    }
+    return result
 }
 
-/*
-* 将某个对象的name属性的每个单词首字母大写并返回该对象
-*/
-function nameToUpperCase<T extends hasNameProperty>(obj: T): T {
-    obj.name = obj.name
-        .split(' ')
-        .map(n => n[0].toUpperCase() + n.substr(1))
-        .join(" ")
-    return obj
-}
+const result = mixinArray([1, 3, 4], ["a", "b", "c"]);
 
-const o = {
-    name: 'guange yang',
-    age: 18,
-    gender: 'man'
-}
-
-console.log(nameToUpperCase(o))
+result.forEach(r => {
+    console.log(r)
+})
