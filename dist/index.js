5112bd6faa9cd139518d6582be41644da47d3120
yangguanghe <1649063797@qq.com>
2021-07-01 23:27:47 +0800

    this指向约束

--- dist/index.js
+++ dist/index.js
@@ -1,32 +1,18 @@
-const obj = {
-    name: 'asdf',
-    age: 12,
-    'my-pid': '131424'
+const u = {
+    name: 'yang',
+    age: 18,
+    sayHello() {
+        console.log(this.name, this.age, this);
+    }
 };
-const methodsFun = 'sayhello';
 class User {
     constructor(name, age) {
         this.name = name;
         this.age = age;
     }
-    [methodsFun]() {
-        console.log('1');
+    sayHello() {
+        console.log(this, this.name, this.age);
     }
 }
-const u = new User('yang', 18);
-u[methodsFun]();
-console.log(u["pid"]);
-class MyArray {
-    constructor() {
-        this[0] = 1;
-        this[1] = 'a';
-        this[2] = 'yang';
-    }
-}
-const myArr = new MyArray();
-console.log(myArr[0]);
-console.log(myArr[6] = 12);
-const arr = [];
-arr[0] = 1;
-arr["0"] = 3;
-console.log(arr[0]);
+const U = new User('yang', 18);
+U.sayHello();
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,36 @@
-const obj = {
-    name: 'asdf',
-    age: 12,
-    'my-pid': '131424'
+const u:IUser = {
+    name: 'yang',
+    age: 18,
+    sayHello() {
+        console.log(this.name, this.age, this)
+    }
 }
-const methodsFun = 'sayhello'
 
-
-// for (const key in obj) {
-//     console.log(key, obj[key])
-// }
+// const say = u.sayHello
+// say()
 
 class User {
-    // 索引器
-    // 可以这样写 [prop: string]: string | number | {():void}
-    [prop: string]: any
     constructor(
         public name: string,
         public age: number
     ) {}
-    [methodsFun]() {
-        console.log('1')
+    sayHello(this: User) {
+        console.log(this, this.name, this.age)
     }
 }
-const u = new User('yang', 18)
-u[methodsFun]()
 
-console.log(u["pid"])
+const U = new User('yang', 18)
+// 未约束时，这种调用方式就存在报错
+// const Say = U.sayHello
+// Say()
+U.sayHello()
 
-class MyArray {
-    [prop: number]: any
-    0 = 1
-    1 = 'a'
-    2 ='yang'
+
+interface IUser {
+    name: string,
+    age: number,
+    sayHello(this: IUser): void
 }
-
-const myArr = new MyArray()
-console.log(myArr[0])
-console.log(myArr[6] = 12)
-
-
-const arr: any[] = []
-arr[0] = 1
-arr["0"] = 3
-console.log(arr[0])
+// function hello() {
+//     console.log(this, this.name, this.age)
+// }
