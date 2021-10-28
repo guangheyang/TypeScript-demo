65b83447a0807a15d619bfa1a4d363e9dbf7da17
yangguanghe <1649063797@qq.com>
2021-10-29 00:21:16 +0800

    三个关键字

--- dist/index.js
+++ dist/index.js
@@ -1,38 +1,27 @@
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-class MyMath {
-    sum(a, b) {
-        return a + b;
-    }
-}
-__decorate([
-    __param(1, t),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", [Number, Number]),
-    __metadata("design:returntype", void 0)
-], MyMath.prototype, "sum", null);
-function t(target, method, index) {
-    console.log(target, method, index);
-}
-require("reflect-metadata");
 class User {
 }
-__decorate([
-    Reflect.metadata('a', 'b'),
-    __metadata("design:type", String)
-], User.prototype, "loginId", void 0);
-__decorate([
-    Reflect.metadata('a', 'b'),
-    __metadata("design:type", Number)
-], User.prototype, "age", void 0);
+const u1 = new User();
+const u2 = u1;
+const A = User;
+function createUser(cls) {
+    return new cls();
+}
+const u = createUser(User);
+function printUserProp(obj, prop) {
+    console.log(obj[prop]);
+}
+const u3 = {
+    loginId: "123",
+    loginPwd: "abc",
+    age: 10
+};
+printUserProp(u3, 'age');
+const u4 = {
+    loginId: '1243',
+    loginPwd: '1233',
+    age: 12
+};
+const u5 = {
+    title: '文章',
+    date: new Date()
+};
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,66 @@
-// 参数装饰器
-class MyMath{
-  sum(a: number, @t b: number) {
-    return a + b;
-  }
-}
-
-function t(target: any, method: string, index: number) {
-  console.log(target, method, index)
-}
-
-// 关于TS自动注入的元数据
-import "reflect-metadata"
+// typeof
 class User {
-  @Reflect.metadata('a', 'b')
   loginId: string
-  @Reflect.metadata('a', 'b')
-  age: number
+  loginPwd: string
 }
 
-// AOP
-class RegUser {
+const u1 = new User()
+const u2 = u1;
+const A = User;
+
+// new() => USer
+function createUser(cls: typeof User): User {
+  return new cls()
+}
+
+const u = createUser(User)
+
+// keyof
+interface User1 {
   loginId: string
   loginPwd: string
   age: number
-  pid: string
-  email: string
-  /**
-   * 将用户保存到数据库
-   */
-  save() {
-    // 验证处理
+}
 
-    // 通过加入数据库
-  }
+function printUserProp(obj:User, prop: keyof User1) {
+  console.log(obj[prop])
+}
+
+const u3: User1 = {
+  loginId: "123",
+  loginPwd: "abc",
+  age: 10
+}
+
+printUserProp(u3, 'age')
+
+// in
+type Obj = {
+  // [p: string]: string
+  // [p in 'loginId' | 'loginPwd' | 'age']: string 
+  // [p in keyof User1]: string
+  // [p in keyof User1]: User1[p]
+  readonly [p in keyof User1]: User1[p]
+}
+
+const u4: Obj = {
+  loginId: '1243',
+  loginPwd: '1233',
+  age: 12
+}
+// u4.age = '123'
+
+
+type Article = {
+  title: string
+  date: Date
+}
+
+type Pratial<T> = {
+  readonly [p in keyof T] ?: T[p]
+}
+
+const u5: Partial<Article> = {
+  title: '文章',
+  date: new Date()
 }
